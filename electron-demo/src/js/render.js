082bad02d4614c400400fd45953e2e079ab4b383
Sunweijun333 <2318085206.qq.com>
2021-07-31 22:10:18 +0800

    test fs to write file

--- electron-demo/src/js/render.js
+++ electron-demo/src/js/render.js
@@ -2,3 +2,12 @@ let filesystem = require('fs')
 
 console.log('Hello NodeJs');
 console.log(filesystem);
+
+filesystem.writeFile('fstest.txt', 'Hello Node.js', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('写入成功');
+    }
+})
+
