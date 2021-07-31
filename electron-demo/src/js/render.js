ff8c83fe7dc94315269a8372ff3c685546b3ee75
Sunweijun333 <2318085206.qq.com>
2021-07-31 22:22:38 +0800

    add btn click to create file by fs

--- electron-demo/src/js/render.js
+++ electron-demo/src/js/render.js
@@ -11,3 +11,17 @@ filesystem.writeFile('fstest.txt', 'Hello Node.js', (err) => {
     }
 })
 
+let btnForCreateFile = document.querySelector('button');
+
+let i = 1;
+btnForCreateFile.onclick = function() {
+    filesystem.writeFile(`fstest${i}.txt`, `File${i}: Hello Node.js`, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            i++;
+            console.log('写入成功');
+        }
+    })
+    
+} 
